04aaf1d54107886222232aab7e27e9e0a1106da2
Tibor Fazekas <tibcso0322@gmail.com>
2023-10-08 13:45:09 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import Home from './pages/Home'
 import Products from './pages/Products'
 import Cart from './assets/Cart.svg'
 import Trash from './assets/Trash.svg'
@@ -11,10 +10,8 @@ import Trash from './assets/Trash.svg'
 
 export default function App() {
   const [cart, setCart] = useState<Product[]>([]);
-  const [index, setIndex] = useState<number>(1);
   //const [active, setActive] = useState(false);
   const [price, setPrice] = useState('0');
-  const pages = [Home(), Products()];
 
   function formatTitle(original: string): string {
     const MAX = 25
@@ -34,23 +31,21 @@ export default function App() {
 
   return <div className='w-screen h-screen relative bg-gray-400 overflow-hidden'>
     <header className='w-full h-20 bg-gray-600 flex items-center justify-evenly'>
-      <button onClick={() => setIndex(0)} className='text-white text-xl tracking-widest'>Home</button>
-      <button onClick={() => setIndex(1)} className='text-white text-xl tracking-widest'>Products</button>
       <div className='group relative w-30 h-30 cursor-pointer' /* onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} */>
         <img src={Cart} width='50' height='50' alt="" />
-        <span style={{ backgroundColor: cart.length ? 'rgb(74, 222, 128)': '#FFCC70' }} className='absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2 opacity-50 rounded-md text-center px-2 transition-[.2s] group-hover:opacity-0 select-none'>{cart.length ? price + '$': 'Empty'}</span>
+        <span style={{ backgroundColor: cart.length ? 'rgb(74, 222, 128)': '#FFCC70' }} className='absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2 opacity-50 rounded-md text-center px-2 transition-[.2s] group-hover:opacity-0 select-none'>{cart.length ? '$' + price: 'Empty'}</span>
         <div className='overflow-y-auto opacity-0 transition-[.3s] ease-in group-hover:opacity-100 absolute w-[25rem] h-0 group-hover:h-72 bg-white rounded-md border-2 border-black z-10 max-h-72 -left-40'>
           {/**ide jon minden cart item */}
-          {cart.length < 1 ? <span className='text-gray-500 font-bold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center opacity-75 select-none'>No items yet!</span>: <>{cart.map(({ id, image, price: _price, category, title }) => 
+          {cart.length < 1 ? <span className='text-gray-500 font-bold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center opacity-75 select-none'>No items yet!</span>: <>{cart.map(({ id, image, price: _price, category, title }, idx) => 
             <div className='w-full h-20 border-b-2 flex justify-center items-center relative select-none'>
               <span className='absolute left-3 font-bold text-green-700'>{_price}$</span>
               <span>{formatTitle(title)}</span>
-              <img className='absolute right-10' onClick={() => setCart(prev => prev.filter(i => i.id !== id))} src={Trash} width='30' alt="" />
+              <img className='absolute right-10' onClick={() => setCart((prev) => prev.filter((_, _idx) => _idx !== idx))} src={Trash} width='30' alt="" />
             </div>
           )}</>}
         </div>
       </div>
     </header>
-    {pages[index]}
+    <Products addToCart={(product: Product) => setCart(prev => [...prev, product])}/>
   </div>
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,11 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Products() {
+interface Props {
+	addToCart: (product: Product) => void;
+}
+
+export default function Products({ addToCart }: Props) {
 	const [products, setProducts] = useState<Product[]>([]);
 
 	useEffect(() => {
@@ -11,22 +15,26 @@ export default function Products() {
 		}
 		onLoad();
 	}, [])
-	return <div className='relative left-1/2 top-10 -translate-x-1/2 w-[90%] h-[80%] bg-white rounded-xl flex gap-6 flex-wrap items-center justify-evenly overflow-y-scroll'>
-		{products.map(({ category, description, id, image, price, rating, title }) => 
+	return <div className='relative left-1/2 -translate-x-1/2 w-[90%] h-full bg-white rounded-xl flex gap-6 flex-wrap items-center justify-evenly overflow-y-scroll'>
+		{products.map(({ category, id, image, price, rating, title }: Product, idx: number) => 
 			<div className='w-[30rem] h-72 rounded-md mt-10' key={id}>
 				<div className='w-full h-[70%] flex justify-center items-center'>
-					<img src={image} width='120' alt="" />
+					<img src={image} width='120' alt="Image for" className='hover:w-[140px] transition-[.2s] cursor-pointer' />
 				</div>
 				<div className='w-full h-[30%] flex flex-col items-start'>
 					<span className='font-bold'>{title}</span>
 					<span>{category}</span>
-					<span className='flex justify-center items-center'>
-						{rating.rate} 
-						<img className='inline ml-2' src="https://www.svgrepo.com/show/513354/star.svg" width='20' alt="" />
-						/ {rating.count}
+					<span className='flex justify-between items-center relative w-full'>
+						<span>
+							{rating.rate} 
+							<img className='inline ml-2' src="https://www.svgrepo.com/show/513354/star.svg" width='20' alt="" />
+							/ {rating.count}
+						</span>
+						<button onClick={() => addToCart(products[idx])} className='w-fit h-10 select-none border-2 rounded-xl border-lime-600 transition-[.2s] hover:bg-lime-50'><span className='p-4'>${price}</span></button>
 					</span>
 				</div>
 			</div>
 		)}
+		<div className='w-full h-44'></div>
 	</div>
 }
