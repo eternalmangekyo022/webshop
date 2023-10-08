87aaaa06bd6387e9e879d75fedf5ac27b4386ea9
Tibor Fazekas <tibcso0322@gmail.com>
2023-10-08 16:01:50 +0200

    cleanup, added amount management

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Products from './pages/Products'
 import Cart from './assets/Cart.svg'
 import Trash from './assets/Trash.svg'
@@ -9,9 +9,10 @@ import Trash from './assets/Trash.svg'
 // https://fakestoreapi.com/products?limit=8
 
 export default function App() {
-  const [cart, setCart] = useState<Product[]>([]);
-  //const [active, setActive] = useState(false);
+  const [cart, setCart] = useState<{ product: Product, amount: number }[]>([]);
   const [price, setPrice] = useState('0');
+  const [dollar, setDollar] = useState<{ x: number, y: number } | null>(null);
+  const timeout = useRef<number | null>(null);
 
   function formatTitle(original: string): string {
     const MAX = 25
@@ -21,8 +22,8 @@ export default function App() {
   useEffect(() => {
     const getPrice = () => {
       let final = 0;
-      cart.forEach(i => final += i.price)
-      return final.toFixed(3);
+      cart.forEach(({ product: { price }, amount }) => final += price * amount)
+      return final.toFixed(2);
     }
 
     setPrice(getPrice())
@@ -30,22 +31,32 @@ export default function App() {
 
 
   return <div className='w-screen h-screen relative bg-gray-400 overflow-hidden'>
+    {dollar && <div className='dollar absolute z-10 select-none text-green-500 font-bold text-4xl' style={{ left: dollar.x + 'px', top: dollar.y + 'px' }}>$</div> }
     <header className='w-full h-20 bg-gray-600 flex items-center justify-evenly'>
-      <div className='group relative w-30 h-30 cursor-pointer' /* onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} */>
+      <div className='group relative w-30 h-30 cursor-pointer'>
         <img src={Cart} width='50' height='50' alt="" />
         <span style={{ backgroundColor: cart.length ? 'rgb(74, 222, 128)': '#FFCC70' }} className='absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2 opacity-50 rounded-md text-center px-2 transition-[.2s] group-hover:opacity-0 select-none'>{cart.length ? '$' + price: 'Empty'}</span>
-        <div className='overflow-y-auto opacity-0 transition-[.3s] ease-in group-hover:opacity-100 absolute w-[25rem] h-0 group-hover:h-72 bg-white rounded-md border-2 border-black z-10 max-h-72 -left-40'>
+        <div className='cursor-default overflow-y-auto opacity-0 transition-[.3s] ease-in group-hover:opacity-100 absolute w-[25rem] h-0 group-hover:h-72 bg-white rounded-md border-2 border-black z-10 max-h-72 -left-40'>
           {/**ide jon minden cart item */}
-          {cart.length < 1 ? <span className='text-gray-500 font-bold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center opacity-75 select-none'>No items yet!</span>: <>{cart.map(({ price: _price, title }, idx) => 
+          {cart.length < 1 ? <span className='text-gray-500 font-bold absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center opacity-75 select-none'>No items yet!</span>: <>{cart.map(({ product: { price: _price, title }, amount }, idx) => 
             <div className='w-full h-20 border-b-2 flex justify-center items-center relative select-none'>
-              <span className='absolute left-3 font-bold text-green-700'>{_price}$</span>
+              <div className='absolute h-6 w-32 border-2 left-1/2 top-[65%] -translate-x-1/2 flex justify-center items-center'>
+                <button className='w-1/3' disabled={amount === 1} onClick={() => setCart(prev => prev.map((i, index) => index === idx ? {...i, amount: amount - 1}: i))}>-</button>
+                <span className='w-1/3 text-center'>{amount}</span>
+                <button className='w-1/3' onClick={() => setCart(prev => prev.map((i, index) => index === idx ? {...i, amount: amount + 1}: i))}>+</button>
+              </div>
+              <span className='absolute left-3 font-bold text-green-700'>{(_price * amount).toFixed(2)}$</span>
               <span>{formatTitle(title)}</span>
-              <img className='absolute right-10' onClick={() => setCart((prev) => prev.filter((_, _idx) => _idx !== idx))} src={Trash} width='30' alt="" />
+              <img className='absolute right-10 cursor-pointer' onClick={() => setCart((prev) => prev.filter((_, _idx) => _idx !== idx))} src={Trash} width='30' alt="" />
             </div>
           )}</>}
         </div>
       </div>
     </header>
-    <Products addToCart={(product: Product) => setCart(prev => [...prev, product])}/>
+    <Products animateTo={(x, y) => {
+      if(dollar) {setDollar(null); clearTimeout(timeout.current!)}
+      setDollar({ x, y })
+      timeout.current = setTimeout(() => setDollar(null), 1000)
+    }} addToCart={(product: Product) => setCart(prev => prev.map(i => i.product.id).includes(product.id) ? prev.map(i => i.product.id === product.id ? { ...i, amount: i.amount + 1 }: i): [...prev, { product, amount: 1 }])}/>
   </div>
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
+import Cart from '../assets/CartBuy.svg'
+import sample from '../../sample.json'
 
 interface Props {
 	addToCart: (product: Product) => void;
+	animateTo: (x: number, y: number) => void
 }
 
-export default function Products({ addToCart }: Props) {
+export default function Products({ addToCart, animateTo }: Props) {
 	const [products, setProducts] = useState<Product[]>([]);
 
 	useEffect(() => {
-		async function onLoad(): Promise<void> {
+		/**async function onLoad(): Promise<void> {
 			const { data } = await axios.get<Product[]>('https://fakestoreapi.com/products', { headers: { 'Content-Type': 'application/json' } })
 			setProducts(data);
-		}
-		onLoad();
+		} */
+		//onLoad();
+		setProducts(sample)
 	}, [])
 	return <div className='relative left-1/2 -translate-x-1/2 w-[90%] h-full bg-white rounded-xl flex gap-6 flex-wrap items-center justify-evenly overflow-y-scroll'>
 		{products.map(({ category, id, image, price, rating, title }: Product, idx: number) => 
@@ -24,13 +28,19 @@ export default function Products({ addToCart }: Props) {
 				<div className='w-full h-[30%] flex flex-col items-start'>
 					<span className='font-bold'>{title}</span>
 					<span>{category}</span>
-					<span className='flex justify-between items-center relative w-full'>
+					<span className='flex justify-between items-center relative w-full h-10'>
 						<span>
 							{rating.rate} 
 							<img className='inline ml-2' src="https://www.svgrepo.com/show/513354/star.svg" width='20' alt="" />
 							/ {rating.count}
 						</span>
-						<button onClick={() => addToCart(products[idx])} className='w-fit h-10 select-none border-2 rounded-xl border-lime-600 transition-[.2s] hover:bg-lime-50'><span className='p-4'>${price}</span></button>
+						<button onClick={({ pageX: x, pageY: y }) => {
+							addToCart(products[idx])
+							animateTo(x, y)
+						}} className='w-fit h-10 select-none border-2 rounded-xl border-lime-600 transition-[.2s] hover:bg-lime-300 hover:h-14 overflow-x-none group relative'>
+							<span className='relative p-4 transition-[.2s] group-hover:opacity-0'>${price}</span>
+							<img className='flex justify-center items-center font-bold text-3xl opacity-0 group-hover:opacity-100 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' src={Cart} width='30' alt='Cart' />
+						</button>
 					</span>
 				</div>
 			</div>
